0e113b1ccdd3c10814d238c451e57ab6e57c5c93
石黒雄嗣 <ishiguroyushi@mechno.local>
2019-04-28 15:01:47 +0900

    トップのみ表示　タイトル表示

---
+++ footer.php
@@ -0,0 +1,66 @@
+	<footer id="footer">
+				<div class="flex">
+					<div>
+						<dl>
+							<dt class="bold"><a href="shop/">SHOP</a></dt>
+							<dd><a href="shop/#miyagi">宮城県</a></dd>
+							<dd><a href="shop/#yamagata">山形県</a></dd>
+							<dd><a href="shop/#nigata">新潟県</a></dd>
+							<dd><a href="shop/#toyama">富山県</a></dd>
+							<dd><a href="shop/#ishikawa">石川県</a></dd>
+							<dd><a href="shop/#fukui">福井県</a></dd>
+							<dd><a href="shop/#nagano">長野県</a></dd>
+							<dd><a href="shop/#kyoto">京都府</a></dd>
+							<dd><a href="shop/#osaka">大阪府</a></dd>
+							<dd><a href="shop/#hyogo">兵庫県</a></dd>
+						</dl>
+					</div>
+					<div>
+						<dl>
+							<dt class="bold"><a href="guide/">GUIDE</a></dt>
+							<dd><a href="#">ホームセンター</a></dd>
+							<dd><a href="guide/shizai/">建築資材</a></dd>
+							<dd><a href="#">工具・金物</a></dd>
+							<dd><a href="#">塗料・塗料用品</a></dd>
+							<dd><a href="#">インテリア・収納</a></dd>
+							<dd><a href="#">家庭用品・日用品</a></dd>
+							<dd><a href="#">家電・照明</a></dd>
+							<dd><a href="#">ガーデンセンター</a></dd>
+							<dd><a href="#">ペットセンター</a></dd>
+							<dd><a href="#">ムサシプロ</a></dd>
+							<dd><a href="#">ムサシ食品館</a></dd>
+						</dl>
+					</div>
+					<div>
+						<dl>
+							<dt class="bold"><a href="http://www.arcland.co.jp/">HOLDINGS</a></dt>
+							<dd><a href="http://nicopet.jp/">ニコペット</a></dd>
+							<dd><a href="#">ムサシプロ／住DEPOT</a></dd>
+							<dd><a href="http://arc-oasis.com/">ARC OASIS</a></dd>
+							<dd><a href="http://arc-style.jp/">アークスタイル</a></dd>
+							<dd><a href="http://www.arcdrug.co.jp/">アークドラッグ</a></dd>
+							<dd><a href="https://www.arcland.shop/">オンラインショップ</a></dd>
+						</dl>
+					</div>
+					<div>
+						<dl>
+							<dt class="bold"><a href="http://www.arcland.co.jp/">COMPANY</a></dt>
+							<dd><a href="http://www.arcland.co.jp/">アークランドサカモト</a></dd>
+							<dd><a href="#">プライバシーポリシー</a></dd>
+						</dl>
+					</div>
+				</div>
+				<p>
+					<small>Copyright © 2018 ARCLAND SAKAMOTO CO.,LTD. ALL RIGHTS RESERVED.</small>
+				</p>
+			</footer>
+			<!--footer-->
+		</div>
+		<!-- wrapper -->
+		<!-- <script src="js/jquery-2.1.1.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/zdo_drawer_menu.js"></script> -->
+		<!-- <script src="js/func_slick.js"></script> -->
+		<?php wp_footer(); ?>
+	</body>
+</html>
